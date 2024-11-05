daa452a4f8bfd9f8cdd04f6e3b353ba6e61a5776
flowz0 <billylflowers@gmail.com>
2024-11-05 13:15:05 -0800

    pause song currently being played

--- script.js
+++ script.js
@@ -106,6 +106,12 @@ const playSong = (id) => {
     audio.play();
 };
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    playButton.classList.remove('playing');
+    audio.pause();
+};
+
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
         return `
@@ -138,6 +144,8 @@ playButton.addEventListener('click', () => {
         playSong(userData?.currentSong.id);
     }
 });
+
+pauseButton.addEventListener('click', pauseSong);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
