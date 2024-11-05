3b3da6d087d3d7a7a67757c2eb56c32b68090496
flowz0 <billylflowers@gmail.com>
2024-11-05 14:25:50 -0800

    Function to shuffle songs in the playlist with state management updates after shuffling

--- script.js
+++ script.js
@@ -136,6 +136,17 @@ const playPreviousSong = () => {
     }
 };
 
+const shuffle = () => {
+    userData?.songs.sort(() => Math.random() - 0.5);
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+
+    renderSongs(userData?.songs);
+    pauseSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
+};
+
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById('player-song-title');
     const songArtist = document.getElementById('player-song-artist');
@@ -206,6 +217,8 @@ pauseButton.addEventListener('click', pauseSong);
 nextButton.addEventListener('click', playNextSong);
 
 previousButton.addEventListener('click', playPreviousSong);
+
+shuffleButton.addEventListener('click', shuffle);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
